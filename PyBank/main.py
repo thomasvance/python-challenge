9cab0c66a340931844df714af9fb762baccdee82
thomasvance <thomasvance@hotmail.com>
2024-07-14 15:09:17 -0400

    V.1.1 This will be the final update for the code.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,113 @@
-this is a TabError
+
+import os
+import csv
+
+#used to import the path of the csv file to be read.
+csvpath = os.path.join("Resources", "budget_data.csv")
+
+#lists that will store the value of both rows. different values for the second column as well.
+profit = []
+average = []
+diff = {}
+prev = 0
+nex = 0
+total = 0
+#reads the csv file
+with open(csvpath, encoding = "utf-8") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    #skips the first row in the csv file.
+    next(csvreader)
+    
+
+
+    
+
+    for row in csvreader:
+
+        #sets nex as the first value in the row. Will not be put into a list, 
+        #but will be used to find the difference between it and the next row.
+        nex = int(row[1])
+
+        #prev starts at 0 so it skips the first row to calculate the absolute difference.
+        if prev == 0:
+            pass
+        else:
+            #this compares the absolute value between the prev and nex, if nex is a negative,
+            #and prev is a positive, it will calculate the absolute value as a negative decrease.
+            if prev > nex and int(row[1]) < 0:
+                inc = (abs(prev - nex))
+                diff.update({row[0]: -inc})
+                average.append(-inc)
+                
+            #this compares the absolute value between the prev and nex, if nex is a positive,
+            #and prev is a positive, but nex is bigger it will calculate the absolute value as a positive increase.
+            if prev < nex and int(row[1]) > 0:
+                inc = (abs(prev - nex))
+                diff.update({row[0]: inc})
+                average.append(inc)
+                
+            #this compares the absolute value between the prev and nex, if prev is greater than nex,
+            #and nex is positive, but nex is smaller then it will be a postive decrease.
+            if prev > nex and int(row[1]) > 0:
+                inc = (abs(prev - nex))
+                diff.update({row[0]: -inc})
+                average.append(-inc)
+                
+            #this compares the absolute value between the prev and nex, if prev is greater than nex,
+            #and if nex is a negative, but nex is a bigger number then it is a negative increase
+            if prev < nex and int(row[1]) < 0: 
+                inc = (abs(prev - nex))
+                diff.update({row[0]: inc})
+                average.append(inc)
+
+                
+        #list updated to represent all of the profit/loss as an int.
+        profit.append(int(row[1]))
+        #sets the prev value for each for loop.
+        prev = int(row[1])
+        #adds all of the profit/loss together to create the total.
+        total = sum(profit)   
+
+#diff is added and updated during the for loop, adding the date to the increase/decrease value.
+#this statement grabs the highest number of profit gain.
+maximum = max(diff, key = diff.get)
+
+#this statement grabs the lowest amount of profit loss. 
+minimum = min(diff, key = diff.get)
+
+#this statement is used to find the average value of the absolute value difference added into average.
+list(map(int, average))
+mid = sum(average) / len(average)
+
+#takes the value given from the average statement abov and reduces the decimals and rounds up to the 3rd decimal.
+change = (round(mid, 2))
+
+
+#statement creates a path and allows for the creation of the output.txt,
+#all print statements will be written to output.txt
+save_path = "Analysis"
+name = ("output.txt")
+txtpath = os.path.join(save_path, name)
+f = open(txtpath, "w")
+   
+print("", file = f)
+print("Financial Analysis", file = f)
+print("",file = f)
+print("-----------------------------", file = f)
+print("", file = f)
+print(f"Total Months:", (len(profit)), file = f)
+print("", file = f)
+print("Total:", ("$" + str(total)), file = f)
+print("", file = f)
+print(f"Average Change:", "$" + (str(change)), file = f)
+print("", file = f)
+print(f"The Greatest in Increase:", maximum, "(" + "$" + str(diff[maximum]) + ")", file = f)
+print("", file = f)
+print(f"The Greatest in Decrease:", minimum, "(" + "$" + str(diff[minimum]) + ")", file = f)
+    
+f.close()
+
+#this will read the results of the output.txt value in the terminal.
+with open(txtpath, "r") as f:
+    data = f.read()
+    print(data)
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # python-challenge
+
+#Updated code for PyBank and PyPoll:
+
+	PyBank: most code there was created and thought of by me and the previous works I've done.
+
+	code on line 79 i found the idea to use list and map from: https://stackoverflow.com/questions/7368789/convert-all-strings-in-a-list-to-integers
+	given by cheeken.
+
+
+	(https://stackoverflow.com/questions/62909803/how-to-store-output-printed-on-terminal-console-to-a-text-file-in-python from aleksander ikleiw) gave me the idea to use
+	the code starting on line 91 - 106 giving me the ideal on using the (, file = f), the other code i experimented with because the Analysis folder would not be found via 
+	("Analysis", "Output.txt", "w"). so trial and error lead me to the code set up that i have now and having it sent out to the output.txt, using previous similar code i 	found a way to print the code into the terminal. This way you can have the output and have the results show in terminal.
+
+
+
+	PyPoll: most of the code I figured out on my own or used from what i learned during PyBank.
+	
+	the list(set(list)) code i found at (https://stackoverflow.com/questions/12897374/get-unique-values-from-a-list-in-python by lefterav).
+	Showed me iterable class (set()). a little bit of trial and error work out how it actually works. 
